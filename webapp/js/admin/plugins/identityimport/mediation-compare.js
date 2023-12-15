773b4d46bbbbcbcfeb5beb9e90e7ec3733910bc6
Clément Roure <croure@yupiik.com>
2023-12-15 12:51:30 +0200

    LUT-24948 - FIX javascript merge attributes

--- webapp/js/admin/plugins/identityimport/mediation-compare.js
+++ webapp/js/admin/plugins/identityimport/mediation-compare.js
@@ -116,10 +116,12 @@ export default class MediationCompare {
             });
         } else {
             inputNames.forEach((name) => {
-                const inputsToDelete = this.mergeForm.querySelectorAll(`input[name="${name}"]`);
-                inputsToDelete.forEach(input => {
-                    this.mergeForm.removeChild(input);
-                });
+                const formChildren = Array.from(this.mergeForm.children);
+                for (let formChild of formChildren) {
+                    if (formChild.getAttribute("name") === name) {
+                        this.mergeForm.removeChild(formChild);
+                    }
+                }
             });
         }
     }
